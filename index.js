da5295e55c9dd3e234fd5a157bd08adc8791b3dc
Tate <tatesmith02@gmail.com>
2018-06-11 17:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 function happyHolidays(){
   return "Happy Holidays!";
 }
+happyHolidays();
 
 function happyHolidaysTo(name){
   return 'Happy holidays, ${name}';
 }
+happyHolidaysTo("Tate");
 
 function happyHolidayTo(holiday, name){
   return 'Happy ${holiday}, ${name}!';
 }
+happyHolidayTo('Fourth-of-July', 'Family');
+
+function holidayCountdown(name, days){
+  return 'Its ${days} days until ${holiday}!';
+}
+holidayCountdown('Family and Friends', 5);
